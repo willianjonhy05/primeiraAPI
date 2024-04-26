cfe6347e8bfb5a78ebea095ebb511aacde7ebd05
willianjonhy05 <willianjohny05@gmail.com>
2024-04-25 21:12:06 -0300

    Alterações

--- setupAPI/loginJwt/models.py
+++ setupAPI/loginJwt/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
+
 
 
 class Question(models.Model):
@@ -10,3 +13,33 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+class User(AbstractUser):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=1000)
+    bio = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='user_images', default='default.jpg')
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.full_name
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
--- setupAPI/loginJwt/views.py
+++ setupAPI/loginJwt/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    context = {}
-    return render(request, 'index.html', context)
+    return HttpResponse('')
+
+
